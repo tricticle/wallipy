8ee6fffa64ffe1444a34748af7e55203da0619a9
tricticle <tricticle@gmail.com>
2023-04-25 00:40:17 +0530

    added new proj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './script';
 
-function App() {
+const MyComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="nav-tab">
+        <div className="image-text">
+          <span className="image">
+            <img src="icon.png" alt="recreit" />
+          </span>
+        </div>
+        <div className="menu-bar">
+          <div className="menu">
+            <a href="https://anilist.co/user/tricticle/">
+              <i className="fa-solid fa-heart"></i>
+              <span className="anime">anime</span>
+            </a>
+            <a href="https://open.spotify.com/user/316utwgq5edsqyr5h43eous2mcjq?si=5e027e2dae584ffa">
+              <i className="fa-brands fa-spotify"></i>
+              <span className="spotify">spotify</span>
+            </a>
+            <a href="https://www.instagram.com/tricticle/">
+              <i className="fa-brands fa-instagram"></i>
+              <span className="">instagram</span>
+            </a>
+            <a href="https://www.youtube.com/channel/UCdIteieIkYQzUdsxTkWCfwg">
+              <i className="fa-brands fa-youtube"></i>
+              <span className="youtube">youtube</span>
+            </a>
+            <a href="https://www.reddit.com/user/tricticle">
+              <i className="fa-brands fa-reddit"></i>
+              <span className="reddit">reddit</span>
+            </a>
+            <a href="https://www.pixiv.net/en/users/76712418">
+              <i className="fa-solid fa-paintbrush"></i>
+              <span className="pixiv">pixiv</span>
+            </a>
+          </div>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="art"></div>
+        <form id="subredditForm">
+          <input type="text" id="subredditInput" placeholder="subreddit name" />
+          <input type="submit" value="Generate Art" />
+          <input type="button" id="saveArtButton" value="Save Art" />
+        </form>
+      </div>
+      <footer className="about-page">
+        <h6>2023 copyright to tricticle</h6>
+        <p>all Generated images[arts] credits goes to <a href="https://www.reddit.com/">creators</a></p>
+      </footer>
     </div>
   );
-}
+};
 
-export default App;
+export default MyComponent;
