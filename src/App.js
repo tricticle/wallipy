bbb90455d393c528840716fb5d6bae85b863bceb
tricticle <tricticle@gmail.com>
2023-04-30 01:16:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -53,6 +53,41 @@ function App() {
 
   return (
     <>
+        <nav className="nav-tab">
+        <div className="image-text">
+          <span className="image">
+            <img src="assets/icon.png" alt="recreit" />
+          </span>
+        </div>
+        <div className="menu-bar">
+          <div className="menu">
+            <a href="https://anilist.co/user/tricticle/">
+              <i className="fa-solid fa-heart"></i>
+              <span className="anime">anime</span>
+            </a>
+            <a href="https://open.spotify.com/user/316utwgq5edsqyr5h43eous2mcjq?si=5e027e2dae584ffa">
+              <i className="fa-brands fa-spotify"></i>
+              <span className="spotify">spotify</span>
+            </a>
+            <a href="https://www.instagram.com/tricticle/">
+              <i className="fa-brands fa-instagram"></i>
+              <span className="">instagram</span>
+            </a>
+            <a href="https://www.youtube.com/channel/UCdIteieIkYQzUdsxTkWCfwg">
+              <i className="fa-brands fa-youtube"></i>
+              <span className="youtube">youtube</span>
+            </a>
+            <a href="https://www.reddit.com/user/tricticle">
+              <i className="fa-brands fa-reddit"></i>
+              <span className="reddit">reddit</span>
+            </a>
+            <a href="https://www.pixiv.net/en/users/76712418">
+              <i className="fa-solid fa-paintbrush"></i>
+              <span className="pixiv">pixiv</span>
+            </a>
+          </div>
+        </div>
+      </nav>
     <div className="container">
       <div className="art" onClick={handleClick} style={{ backgroundImage: `url(${imageUrls[currentImageIndex]})` }} />
       <form onSubmit={handleSubmit} id="subredditForm">
