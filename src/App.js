9ecc9c60c2824b24d9d6f3f0c4b09d15774db141
tricticle <tricticle@gmail.com>
2023-05-17 11:46:32 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -8,81 +8,119 @@ function App() {
   const [showMessage, setShowMessage] = useState(true);
   const [selectedSubreddit, setSelectedSubreddit] = useState("wallpaper");
   const [artInfo, setArtInfo] = useState({});
+  const [customSubreddit, setCustomSubreddit] = useState("");
 
-
-  const subreddits = ["wallpaper","amoledbackgrounds","midjourneyfantasy","patchuu","imaginarysliceoflife","animeart","moescape","fantasymoe","animewallpaper","awwnime"];
+  const subreddits = [
+    "wallpaper",
+    "amoledbackgrounds",
+    "midjourneyfantasy",
+    "patchuu",
+    "imaginarysliceoflife",
+    "animeart",
+    "moescape",
+    "fantasymoe",
+    "animewallpaper",
+    "awwnime",
+  ];
 
   useEffect(() => {
-    const subreddit = selectedSubreddit;
+    const subreddit = selectedSubreddit === "custom" ? customSubreddit : selectedSubreddit;
     const apiUrl = `https://www.reddit.com/r/${subreddit}.json?sort=hot&limit=99`;
 
-     // Fetch data from Reddit API
-     fetch(apiUrl)
-     .then((response) => response.json())
-     .then((data) => {
-       // Filter URLs of images and their corresponding info
-       const posts = data.data.children.filter(
-         (post) =>
-           post.data.post_hint === "image" && (showNsfw || !post.data.over_18)
-       );
-       const urls = posts.map((post) => post.data.url);
-       const info = posts.map((post) => ({
-         id: post.data.author,
-         title: post.data.title
-       }));
+    // Fetch data from Reddit API
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        // Filter URLs of images and their corresponding info
+        const posts = data.data.children.filter(
+          (post) =>
+            post.data.post_hint === "image" && (showNsfw || !post.data.over_18)
+        );
+        const urls = posts.map((post) => post.data.url);
+        const info = posts.map((post) => ({
+          id: post.data.author,
+          title: post.data.title,
+        }));
 
-       // Set the image URLs and info states
-       setImageUrls(urls);
-       setArtInfo(info);
+        // Set the image URLs and info states
+        setImageUrls(urls);
+        setArtInfo(info);
 
-       // Set the first image as the current index
-       setCurrentImageIndex(0);
-     })
-     .catch((error) => console.error(error));
- }, [selectedSubreddit, showNsfw]);
+        // Set the first image as the current index
+        setCurrentImageIndex(0);
+      })
+      .catch((error) => console.error(error));
+  }, [selectedSubreddit, showNsfw, customSubreddit]);
 
- const handleClick = (event) => {
-   const clickY = event.clientY;
-   const artHeight = event.target.offsetHeight;
-   setShowMessage(false);
+  const handleClick = (event) => {
+    const clickX = event.clientX;
+    const artWidth = event.target.offsetWidth;
+    setShowMessage(false);
+  
+    if (clickX < artWidth / 2) {
+      // Clicked on the left half of the art element, go to the previous image
+      setCurrentImageIndex(
+        (currentImageIndex - 1 + imageUrls.length) % imageUrls.length
+      );
+    } else {
+      // Clicked on the right half of the art element, go to the next image
+      setCurrentImageIndex((currentImageIndex + 1) % imageUrls.length);
+    }
+  };
+  
 
-   if (clickY < artHeight / 2) {
-     // Clicked on the top half of the art element, go to previous image
-     setCurrentImageIndex((currentImageIndex - 1 + imageUrls.length) % imageUrls.length);
-   } else {
-     // Clicked on the bottom half of the art element, go to next image
-     setCurrentImageIndex((currentImageIndex + 1) % imageUrls.length);
-   }
- };
+  const handleSaveClick = () => {
+    // Download the current image
+    const imageUrl = imageUrls[currentImageIndex];
+    const a = document.createElement("a");
+    a.href = imageUrl;
+    a.download = "art.jpg";
+    a.click();
+  };
 
- const handleSaveClick = () => {
-   // Download the current image
-   const imageUrl = imageUrls[currentImageIndex];
-   const a = document.createElement("a");
-   a.href = imageUrl;
-   a.download = "art.jpg";
-   a.click();
- };
+  const handleToggle = () => {
+    setShowNsfw(!showNsfw);
+  };
 
- const handleToggle = () => {
-   setShowNsfw(!showNsfw);
- };
+  const handleSelectChange = (event) => {
+    const value = event.target.value;
+    setSelectedSubreddit(value);
 
- const handleSelectChange = (event) => {
-   setSelectedSubreddit(event.target.value);
- };
+    if (value === "custom") {
+      setShowMessage(false);
+    } else {
+      setShowMessage(true);
+    }
+  };
 
- const currentArtInfo = artInfo[currentImageIndex] || {};
+  const handleCustomSubredditChange = (event) => {
+    setCustomSubreddit(event.target.value);
+  };
 
- return (
+  const currentArtInfo = artInfo[currentImageIndex] || {};
+
+  return (
     <>
-    <section className="header"><h1>wallipy.</h1></section>
+      <section className="header">
+        <h1>wallipy.</h1>
+      </section>
       <div className="container">
-        <div className="art" onClick={handleClick} style={{ backgroundImage: `url(${imageUrls[currentImageIndex]})` }}>
-          <section className="data">
-          {currentArtInfo.title} by <a href="https://www.reddit.com/u/{currentArtInfo.id}">{currentArtInfo.id}</a>
-          </section>
-          {showMessage && <div className="message">Click on the left or right side to change the image</div>}
+        <div
+          className="art"
+          onClick={handleClick}
+          style={{ backgroundImage: `url(${imageUrls[currentImageIndex]})` }}
+        >
+          <div className="data">
+            {currentArtInfo.title} by{" "}
+            <a href={`https://www.reddit.com/u/${currentArtInfo.id}`}>
+              {currentArtInfo.id}
+            </a>
+          </div>
+          {showMessage && (
+            <div className="message">
+              Click on the left or right side to change the image
+            </div>
+          )}
         </div>
         <form id="subredditForm">
           <select value={selectedSubreddit} onChange={handleSelectChange}>
@@ -91,20 +129,39 @@ function App() {
                 {subreddit}
               </option>
             ))}
+            <option value="custom">Custom Subreddit</option>
           </select>
-          <input type="button" id="saveArtButton" value="Save Art" onClick={handleSaveClick} />
+          {selectedSubreddit === "custom" && (
+            <input
+              type="text"
+              value={customSubreddit}
+              onChange={handleCustomSubredditChange}
+              placeholder="Enter subreddit name"
+            />
+          )}
+          <input
+            type="button"
+            id="saveArtButton"
+            value="Save Art"
+            onClick={handleSaveClick}
+          />
           <div className="toggle">
-            <input type="checkbox" id="nsfwToggle" checked={showNsfw} onChange={handleToggle} />
+            <input
+              type="checkbox"
+              id="nsfwToggle"
+              checked={showNsfw}
+              onChange={handleToggle}
+            />
             <label htmlFor="nsfwToggle">Show NSFW Content</label>
           </div>
         </form>
       </div>
       <footer className="about-page">
-          <h6>2023 copyright to tricticle</h6>
-          <p>
-            all Generated images[arts] credits goes to <a href="https://www.reddit.com/">creators</a>
-          </p>
-        </footer>
+        <h6>2023 copyright to tricticle</h6>
+        <p>
+          all Generated images[arts] credits go to <a href="https://www.reddit.com/">creators</a>
+        </p>
+      </footer>
     </>
   );
 }
