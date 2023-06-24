a2c01bbfd49520ee63d4e0c1cedcfa786374e3a7
tricticle <95101379+tricticle@users.noreply.github.com>
2023-06-24 23:30:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
 
   const fetchSubreddits = subredditsToFetch.map((subreddit) => {
     // Fetch images for each subreddit
-    const apiUrl = `https://www.reddit.com/r/${subreddit}.json?sort=hot&limit=1`;
+    const apiUrl = `https://www.reddit.com/r/${subreddit}.json?sort=hot&limit=99`;
     return fetch(apiUrl)
       .then((response) => {
         if (response.ok) {
