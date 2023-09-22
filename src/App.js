e0d9217528eacdc703f0fd2e8d9bad94ba702f1d
tricticle <95101379+tricticle@users.noreply.github.com>
2023-09-23 01:29:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -372,9 +372,7 @@ function AuthenticatedApp() {
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
+      redirectUri={window.location.origin}
     >
       <App />
     </Auth0Provider>
