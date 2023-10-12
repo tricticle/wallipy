d9b91eca588e4fe98bc21c264745e84a49403f03
tricticle <tricticle@gmail.com>
2023-10-12 23:59:35 +0530

    perfect for now

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -2,6 +2,31 @@ import React, { useState, useEffect } from "react";
 
 function HeroSection({ likedImages }) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [touchStartX, setTouchStartX] = useState(null);
+
+  const handleTouchStart = (e) => {
+    setTouchStartX(e.touches[0].clientX);
+  };
+
+  const handleTouchEnd = (e) => {
+    const touchEndX = e.changedTouches[0].clientX;
+    const deltaX = touchEndX - touchStartX;
+
+    if (deltaX > 50) {
+      // Swipe right, go to the previous image
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === 0 ? likedImages.length - 1 : prevIndex - 1
+      );
+    } else if (deltaX < -50) {
+      // Swipe left, go to the next image
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % likedImages.length);
+    }
+  };
+
+  const handleHeroClick = () => {
+    // When the HeroSection is clicked, go to the next image
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % likedImages.length);
+  };
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -14,19 +39,24 @@ function HeroSection({ likedImages }) {
   }, [likedImages]);
 
   return (
-    <section className="container">
-        <div className="cur-sec">
+    <section
+      className="container"
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+      onClick={handleHeroClick} // Click anywhere in the HeroSection to change images
+    >
+      <div className="cur-sec">
         <div className="cur-flex">
-        <div className="cur-art">
-        {likedImages.length > 0 && (
-        <img 
-          src={likedImages[currentImageIndex].imageUrl}
-          alt={likedImages[currentImageIndex].title}
-        />
-      )}
+          <div className="cur-art">
+            {likedImages.length > 0 && (
+              <img
+                src={likedImages[currentImageIndex].imageUrl}
+                alt={likedImages[currentImageIndex].title}
+              />
+            )}
+          </div>
         </div>
-        </div>
-        </div>
+      </div>
     </section>
   );
 }
